b4de18a5645679c78d4fc355543e7d2c3d69668c
iwgx <wahabiputra@gmail.com>
2020-02-10 16:32:52 +0700

    fix: Secret - store in env variable

--- helpers/withAuth.js
+++ helpers/withAuth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const secret = "mysecretsshhh";
+const secret = process.env.SECRET;
 
 const withAuth = function(req, res, next) {
   const token = req.cookies.token;
